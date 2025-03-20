7ea3ac804641c226c444d25e3adec820678762a1
bison-- <mike.bison42@googlemail.com>
2025-03-20 09:22:46 +0100

    all commented

--- addons/AutoSizeText/ui/AutoSizeRichTextLabel.gd
+++ addons/AutoSizeText/ui/AutoSizeRichTextLabel.gd
@@ -83,11 +83,11 @@ func _ready() -> void:
 		autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
 		push_warning("changed autowrap_mode to " + str(autowrap_mode))
 
-	if visible_characters_behavior == TextServer.VC_CHARS_BEFORE_SHAPING:
-		visible_characters_behavior = TextServer.VC_CHARS_AFTER_SHAPING
-		push_warning("changed visible_characters_behavior to " + str(visible_characters_behavior))
+	#if visible_characters_behavior == TextServer.VC_CHARS_BEFORE_SHAPING:
+	#	visible_characters_behavior = TextServer.VC_CHARS_AFTER_SHAPING
+	#	push_warning("changed visible_characters_behavior to " + str(visible_characters_behavior))
 
-	bbcode_enabled = true
+	#bbcode_enabled = true
 
 	resized.connect(do_resize_text)
 	do_resize_text()
@@ -96,8 +96,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if watch_text_change:
 		if text != _last_text:
-			print(Time.get_datetime_string_from_system())
-			print(text + "\n#####\n" + _last_text)
+			#print(Time.get_datetime_string_from_system())
+			#print(text + "\n#####\n" + _last_text)
 			
 			do_resize_text()
 			
@@ -108,8 +108,8 @@ func _process(delta: float) -> void:
 
 
 func resize_text() -> void:
-	#if not needs_resize():
-	#	return
+	if not needs_resize():
+		return
 
 	do_resize_text()
 
@@ -140,13 +140,14 @@ func do_resize_text() -> void:
 		set(&"theme_override_font_sizes/bold_font_size", target_font_size)
 
 		# force a refresh before checking needs_resize
-		custom_minimum_size = Vector2(custom_minimum_size.x, custom_minimum_size.y)
+		#custom_minimum_size = Vector2(custom_minimum_size.x, custom_minimum_size.y)
+		
 		#var string_size = $Label.get_theme_font("font").get_string_size($Label.text, HORIZONTAL_ALIGNMENT_LEFT, -1, $Label.get_theme_font_size("font_size"))
 		#var string_size = get_theme_font("font").get_multiline_string_size(text, horizontal_alignment, -1, target_font_size)
 		#print(string_size)
 		#if not visible_lines(target_font_size):
 		#if not needs_resize():
-		print(get_v_scroll_bar().max_value)
+		#print(get_v_scroll_bar().max_value)
 		if not get_content_height() > get_rect().size.y:
 			break
 	
@@ -156,22 +157,22 @@ func do_resize_text() -> void:
 	#_do_resize_text.call_deferred()
 
 
-func _do_resize_text() -> void:
-	for target_font_size: int in get_iterator():
-		set(&"text", "[font_size={0}]{1}".format([target_font_size, text.substr(text.find("]", 0) + 1, -1)]))
+#func _do_resize_text() -> void:
+#	for target_font_size: int in get_iterator():
+#		set(&"text", "[font_size={0}]{1}".format([target_font_size, text.substr(text.find("]", 0) + 1, -1)]))
+#
+#		if not visible_lines(target_font_size):
+#			break
+#	
+#	_processing_flag = false
 
-		if not visible_lines(target_font_size):
-			break
-	
-	_processing_flag = false
 
-
-func visible_lines(char_size : float) -> bool:
-	char_size = (maxf(char_size,0.01) / 12.0) * 16.0
-	print("########")
-	print(char_size)
-	print(str(get_line_count()) + " / " + str(int(maxf(size.y, 0.01) / (char_size))))
-	return get_line_count() > int(maxf(size.y, 0.01) / (char_size))
+#func visible_lines(char_size : float) -> bool:
+#	char_size = (maxf(char_size,0.01) / 12.0) * 16.0
+#	print("########")
+#	print(char_size)
+#	print(str(get_line_count()) + " / " + str(int(maxf(size.y, 0.01) / (char_size))))
+#	return get_line_count() > int(maxf(size.y, 0.01) / (char_size))
 
 
 func get_iterator() -> Array:
@@ -187,4 +188,5 @@ func get_iterator() -> Array:
 
 
 func needs_resize() -> bool:
-	return (get_line_count() + 1 > get_visible_line_count())
+	#return (get_line_count() + 1 > get_visible_line_count())
+	return get_content_height() > get_rect().size.y
