f1d1bf37b3ad0c2cb9397f3a90bdd0dcfb762846
CNTW_DEV <homiedeveloperscontact@gmail.com>
2025-03-30 20:42:23 -0300

    Added margin;Step Size

--- addons/AutoSizeText/ui/AutoSizeLineEdit.gd
+++ addons/AutoSizeText/ui/AutoSizeLineEdit.gd
@@ -8,7 +8,7 @@ class_name AutoSizeLineEdit
 extends LineEdit
 
 @export_tool_button("FORCE REFRESH")
-var refresh_button: Callable = _on_change_rect
+var refresh_button: Callable = resize_text
 
 @export_group("Auto Font Size")
 
@@ -18,7 +18,7 @@ var min_size: int = 8:
 	set(new_min):
 		min_size = min(max(1, new_min), max_size)
 		if is_node_ready():
-			_on_change_rect()
+			resize_text()
 
 ## Max text size to reach
 @export_range(1, 512)
@@ -26,11 +26,40 @@ var max_size: int = 38:
 	set(new_max):
 		max_size = max(min_size, min(new_max, 512))
 		if is_node_ready():
-			_on_change_rect()
+			resize_text()
 
-# TODO: implement @export_group("Font Step Size")
+@export var use_focus_theme : bool = false:
+	set(use_focus):
+		use_focus_theme = use_focus
+		if use_focus:
+			if !focus_entered.is_connected(update):
+				focus_entered.connect(update)
+			if !focus_exited.is_connected(update):
+				focus_exited.connect(update)
+		else:
+			if focus_entered.is_connected(update):
+				focus_entered.disconnect(update)
+			if focus_exited.is_connected(update):
+				focus_exited.disconnect(update)
+
+@export_group("Step Size")
+
+## Needs 2 numbers to work / will be automatically prefered over "Auto-Size"[br]
+## when 2 numbers or more are present.
+@export
+var step_sizes: Array[int] = []:
+	get:
+		return step_sizes
+	set(value):
+		step_sizes = value
+		step_sizes.sort()
+
+		notify_property_list_changed()
+		resize_text()
+
 
 var _processing_flag: bool = false
+var _setup_flag : bool = false
 
 func _set(property: StringName, _value: Variant) -> bool:
 	if (
@@ -38,38 +67,53 @@ func _set(property: StringName, _value: Variant) -> bool:
 		or property == &"right_icon"
 		or property == &"clear_button_enabled"
 		or property == &"placeholder_text"
+		or property == &"editable"
 	):
-		_on_change_rect.call_deferred()
-		
+		resize_text.call_deferred()
+
 	return false
 
 func _ready() -> void:
-	item_rect_changed.connect(_on_change_rect)
-	_on_change_rect.call_deferred()
+	item_rect_changed.connect(resize_text)
 
-func _on_change_rect() -> void:
+	#Process custom themes on focus
+	if use_focus_theme:
+		if !focus_entered.is_connected(update):
+			focus_entered.connect(update)
+		if !focus_exited.is_connected(update):
+			focus_exited.connect(update)
+
+	resize_text.call_deferred()
+
+func update() -> void:
+	set_process(true)
+
+func _process(delta: float) -> void:
+	resize_text()
+	set_process(false)
+
+func resize_text() -> void:
 	if _processing_flag:
 		return
-		
+
 	_processing_flag = true
-	
+
 	# Taking custom char offset prevent text clip by rect
 	const OFFSET_BY: String = "_"
-	
+
 	var font: Font = get(&"theme_override_fonts/font")
-	var character_size: int = max_size
 	var font_size: Vector2 = Vector2.ZERO
 	var offset: float = 0
-	
+
 	#region kick_falls
-	
+
 	var current_text: String = text
 	var use_clear_btn: bool = clear_button_enabled
-	
+
 	if current_text.is_empty():
 		if placeholder_text.is_empty():
 			return
-			
+
 		current_text = placeholder_text
 		use_clear_btn = false
 
@@ -77,68 +121,87 @@ func _on_change_rect() -> void:
 		font = get_theme_default_font()
 
 	var right_icon_size: float = 0.0
-	
+
 	if null != right_icon:
 		# Assume native size
 		right_icon_size = right_icon.get_size().x
-		
+
 	if use_clear_btn:
 		var _clear: Texture2D = get(&"theme_override_icons/clear")
 		if null == _clear:
 			_clear = get_theme_icon(&"clear")
 		if _clear is Texture2D:
 			right_icon_size += (_clear as Texture2D).get_size().x
-		
+
 	#endregion
 
-	font_size = font.get_string_size(
-		current_text,
-		alignment, 
-		-1,
-		character_size,
-		TextServer.JUSTIFICATION_NONE,
-		TextServer.DIRECTION_AUTO,
-		TextServer.ORIENTATION_HORIZONTAL
-	)
-	font_size.x += right_icon_size
+	var rect_size : float = get_rect().size.x
+	var current_theme : StyleBox = null
 
-	offset = (font.get_string_size(
-		OFFSET_BY,
-		alignment,
-		-1,
-		character_size,
-		TextServer.JUSTIFICATION_NONE,
-		TextServer.DIRECTION_AUTO,
-		TextServer.ORIENTATION_HORIZONTAL
-	)).x
-	
-	while size.x - offset < font_size.x:
-		character_size = character_size - 1
+	if has_focus():
+		current_theme = get(&"theme_override_styles/focus")
+		if current_theme == null:
+			current_theme = get_theme_stylebox(&"focus")
+	elif !editable:
+		current_theme = get(&"theme_override_styles/read_only")
+		if current_theme == null:
+			current_theme = get_theme_stylebox(&"read_only")
+	else:
+		current_theme = get(&"theme_override_styles/normal")
+		if current_theme == null:
+			current_theme = get_theme_stylebox(&"normal")
 
-		if character_size < min_size:
-			character_size = min_size
-			break
+	if current_theme is StyleBoxFlat:
+		rect_size -= current_theme.border_width_left
+		rect_size -= current_theme.border_width_right
+		rect_size -= current_theme.content_margin_left
+		rect_size -= current_theme.content_margin_right
+	elif current_theme is StyleBoxTexture:
+		rect_size -= current_theme.texture_margin_left
+		rect_size -= current_theme.texture_margin_right
+	elif current_theme is StyleBoxEmpty:
+		rect_size -= current_theme.content_margin_left
+		rect_size -= current_theme.content_margin_right
+
+	for font_size_iterator : int in get_iterator():
+		set(&"theme_override_font_sizes/font_size", font_size_iterator)
 
 		font_size = font.get_string_size(
 			current_text,
 			alignment,
 			-1,
-			character_size,
+			font_size_iterator,
 			TextServer.JUSTIFICATION_NONE,
 			TextServer.DIRECTION_AUTO,
 			TextServer.ORIENTATION_HORIZONTAL
 		)
 		font_size.x += right_icon_size
-		
+
 		offset = (font.get_string_size(
 			OFFSET_BY,
 			alignment,
 			-1,
-			character_size,
+			font_size_iterator,
 			TextServer.JUSTIFICATION_NONE,
 			TextServer.DIRECTION_AUTO,
 			TextServer.ORIENTATION_HORIZONTAL
 		)).x
 
-	set(&"theme_override_font_sizes/font_size", character_size)
+		if not needs_resize(rect_size - offset, font_size.x):
+			break
+
 	set_deferred(&"_processing_flag", false)
+
+func needs_resize(rect_size : float, font_size : float) -> bool:
+	return rect_size < font_size
+
+func get_iterator() -> Array:
+	if len(step_sizes) >= 2:
+		var clone: Array[int] = step_sizes.duplicate()
+		clone.reverse()
+		return clone
+
+	if len(step_sizes) == 1:
+		push_warning(name + " Step sizes needs at least 2 numbers to work")
+
+	return range(max_size, min_size, -1)
